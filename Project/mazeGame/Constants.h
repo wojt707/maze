5bcfacb6e0a472655d1e95d6400c5b41e11707d6
wojt707 <dolibog.wojtek@gmail.com>
2023-04-27 13:22:43 +0200

    Constant header

---
+++ Project/mazeGame/Constants.h
@@ -0,0 +1,13 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+#pragma once
+
+const float PI = 3.14159265359f;
+
+const unsigned int CELL_SIZE = 80;
+const unsigned int MIN_MAP_SIZE = 9;
+
+const unsigned int SCREEN_WIDTH = 1600;
+const unsigned int SCREEN_HEIGHT = 800;
+
+#endif
